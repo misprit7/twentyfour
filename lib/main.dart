dcc41c6f5c23120d1774a4042b7c063b267fd6fd
misprit7 <xandernaumenko@gmail.com>
2023-09-27 02:26:40 -0700

    Got all ops working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -42,22 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: ButtonSquare()
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return ButtonSquare();
   }
 }
 
@@ -73,25 +59,25 @@ class _ButtonSquareState extends State<ButtonSquare> {
   int opSelect = -1;
 
   void numberPress(int i) {
-    if(numSelect != -1 || opSelect == -1){
-      if(numbers[i] != -999){
-        setState(() {
-          numSelect = i;
-        });
-      }
+    if(numbers[i] == -999){ return; }
+    if(numSelect == -1 || opSelect == -1){
+      setState(() {
+        numSelect = i;
+      });
       return;
     }
     setState(() {
       if(opSelect == 0){
-        numbers[i] == numbers[numSelect] + numbers[i];
+        numbers[i] = numbers[numSelect] + numbers[i];
       } else if(opSelect == 1){
-        numbers[i] == numbers[numSelect] - numbers[i];
+        numbers[i] = numbers[numSelect] - numbers[i];
       } else if(opSelect == 2){
-        numbers[i] == numbers[numSelect] * numbers[i];
+        numbers[i] = numbers[numSelect] * numbers[i];
       } else if(opSelect == 3){
-        numbers[i] == numbers[numSelect] / numbers[i];
+        numbers[i] = (numbers[numSelect] / numbers[i]).floor();
       }
       numbers[numSelect] = -999;
+      opSelect = -1;
       numSelect = i;
     });
   }
@@ -102,12 +88,22 @@ class _ButtonSquareState extends State<ButtonSquare> {
       return;
     }
     setState((){
-      int reduced = numbers.reduce((a, b){ return i == 4 ? a+b : a*(b == -999 ? 1 : b); });
+      int reduced = numbers.reduce((a, b){ return i == 4 ? a+(b == -999 ? 0 : b) : a*(b == -999 ? 1 : b); });
       for(int i = 0; i < 4; ++i){numbers[i] = -999;}
       if(numSelect != -1){
         numbers[numSelect] = reduced;
       } else {
         numbers[0] = reduced;
+      }
+    });
+  }
+
+  void reset() {
+    setState(() {
+      var rng = Random();
+      for(int i = 0; i < 4; ++i){
+        // non-inclusive and avoid 0
+        numbers[i] = rng.nextInt(13)+1;
       }
     });
   }
@@ -140,33 +136,48 @@ class _ButtonSquareState extends State<ButtonSquare> {
         }).toList(),
       );
     }
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Column(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+      ),
+      body: Center(
+        child: 
+         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            buildButtonRow([(numbers[0].toString(), 0), (numbers[1].toString(), 1)], buttonSize),
-            SizedBox(height: 16.0), // Add horizontal spacing between buttons
-            buildButtonRow([(numbers[2].toString(), 2), (numbers[3].toString(), 3)], buttonSize),
-            SizedBox(height: 16.0), // Add horizontal spacing between buttons
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                buildButtonRow([(numbers[0].toString(), 0), (numbers[1].toString(), 1)], buttonSize),
+                SizedBox(height: 16.0), // Add horizontal spacing between buttons
+                buildButtonRow([(numbers[2].toString(), 2), (numbers[3].toString(), 3)], buttonSize),
+                SizedBox(height: 16.0), // Add horizontal spacing between buttons
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ButtonGroup([('+', 0), ('-', 1), ('×', 2), ('÷', 3)], screenSize.width / 4-16, 8, opPress, opSelect),
+              ],
+            ),
+            SizedBox(height: 16.0), // Add vertical margin
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ButtonGroup([('++', 4), ('××', 5)], screenSize.width / 4-16, 8, opPress, -1),
+              ],
+            ),
           ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ButtonGroup([('+', 0), ('-', 1), ('×', 2), ('÷', 3)], screenSize.width / 4-16, 8, opPress),
-          ],
-        ),
-        SizedBox(height: 16.0), // Add vertical margin
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ButtonGroup([('++', 4), ('××', 5)], screenSize.width / 4-16, 8, opPress),
-          ],
-        ),
-      ],
+        )
+      ),
+      floatingActionButton:
+        FloatingActionButton(
+          onPressed: reset,
+          tooltip: 'Increment',
+          child: const Icon(Icons.restart_alt),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
@@ -225,8 +236,9 @@ class ButtonGroup extends StatelessWidget {
   final double size;
   final double spacing; // Define spacing between buttons
   final void Function(int) onPressed;
+  final int active;
 
-  ButtonGroup(this.labels, this.size, this.spacing, this.onPressed);
+  ButtonGroup(this.labels, this.size, this.spacing, this.onPressed, this.active);
 
   @override
   Widget build(BuildContext context) {
@@ -240,7 +252,7 @@ class ButtonGroup extends StatelessWidget {
           final i = entry.value.$2;
           return Row(
             children: <Widget>[
-              CustomButton(number, size, false, onPressed: (){onPressed(i);}),
+              CustomButton(number, size, active == i, onPressed: (){onPressed(i);}),
               if (index != labels.length - 1) SizedBox(width: spacing), // Add spacing between buttons
             ],
           );
